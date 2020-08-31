92d6c636b82a6c38aa73e1fd549056d49335a6e2
mavennn <agadoev@devors.io>
2020-08-31 22:34:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import createRootReducer from "./store/reducers/rootReducer";
+import { useHistory } from 'react-router';
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import './app.global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
-export default App;
+   const history = useHistory();
+   const rootReducer = createRootReducer(history);
+   const store = createStore(rootReducer, applyMiddleware(thunk));
+
+   return <Root store={store} history={history} />
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
